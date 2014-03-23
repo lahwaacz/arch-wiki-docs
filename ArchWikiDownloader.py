9e83a908bebf1d1b2a358a370dd436db98379663
Jakub Klinkovský <j.l.k@gmx.com>
2014-03-23 18:05:59 +0100

    ArchWikiDownloader: split the downloading stuff into separate module

---
+++ ArchWikiDownloader.py
@@ -0,0 +1,139 @@
+#! /usr/bin/env python
+
+import os.path
+import datetime
+import urllib.request
+
+from simplemediawiki import MediaWiki
+
+from ArchWikiOfflineOptimizer import ArchWikiOfflineOptimizer
+
+class ArchWikiDownloader:
+
+    query_allpages = {
+        "action": "query",
+        "generator": "allpages",
+        "gaplimit": "500",
+        "gapfilterredir": "nonredirects",
+        "gapnamespace": "0",
+        "prop": "info",
+        "inprop": "url",
+        "continue": "",
+    }
+
+    query_allimages = {
+        "action": "query",
+        "list": "allimages",
+        "ailimit": "500",
+        "aiprop": "url|timestamp",
+        "aimaxsize": "10000",
+        "continue": "",
+    }
+
+    css_links = {
+        "https://wiki.archlinux.org/load.php?debug=false&lang=en&modules=mediawiki.legacy.commonPrint%2Cshared%7Cskins.archlinux&only=styles&skin=archlinux&*": "ArchWikiOffline.css",
+    }
+
+    def __init__(self, wikiurl, output_directory, epoch):
+        self.wiki = MediaWiki(wikiurl)
+        self.output_directory = output_directory
+        self.epoch = epoch
+
+        # ensure output directory always exists
+        if not os.path.isdir(self.output_directory):
+            os.mkdir(self.output_directory)
+
+    def query_continue(self, query):
+        while True:
+            result = self.wiki.call(query)
+            if "error" in result:
+                raise Exception(result["error"])
+            if "warnings" in result:
+                print(result["warnings"])
+            if "query" in result:
+                yield result["query"]
+            if "continue" not in result:
+                break
+            query.update(result["continue"])
+
+    def print_namespaces(self):
+        nsmap = self.wiki.namespaces()
+        nsmap[0] = "Main"   # force main namespace to have name instead of empty string
+        print("Available namespaces:")
+        for ns in sorted(nsmap.keys()):
+            print("  %2d -- %s" % (ns, nsmap[ns]))
+
+    def get_local_filename(self, title):
+        """ return file name where the given page should be stored
+        """
+
+        # MediaWiki treats uses '_' in links instead of ' '
+        title = title.replace(" ", "_")
+
+        # handle anomalous titles beginning with '/' (e.g. "/dev/shm")
+        if title.startswith("/"):
+            title = title[1:]
+
+        # pages from File namespace already have an extension
+        if not title.startswith("File:"):
+            title += ".html"
+
+        return os.path.join(self.output_directory, title)
+
+    def needs_update(self, title, timestamp):
+        """ determine if it is necessary to download a page
+        """
+
+        fname = self.get_local_filename(title)
+        if not os.path.exists(fname):
+            return True
+        local = datetime.datetime.utcfromtimestamp(os.path.getmtime(fname))
+        if local < timestamp or local < self.epoch:
+            return True
+        return False
+
+    def process_namespace(self, namespace):
+        """ walk all pages in given namespace, download if necessary
+        """
+
+        print("Processing namespace %s..." % namespace)
+
+        query = self.query_allpages.copy()
+        query["gapnamespace"] = namespace
+        for pages_snippet in self.query_continue(query):
+            for page in pages_snippet["pages"].values():
+                title = page["title"]
+                timestamp = self.wiki.parse_date(page["touched"])
+                if self.needs_update(title, timestamp):
+                    print("  [downloading] %s" % title)
+                    fullurl = page["fullurl"]
+
+                    # FIXME: this is hack to avoid weird caching issues on ArchWiki
+                    # it is probably useful anyway as it provides a permalink in #printfooter
+                    fullurl += "?printable=yes"
+
+                    html = urllib.request.urlopen(fullurl)
+                    awoo = ArchWikiOfflineOptimizer(html, self.get_local_filename(title), self.output_directory)
+                    awoo.optimize()
+                else:
+                    print("  [up-to-date]  %s" % title)
+
+    def download_css(self):
+        print("Downloading CSS...")
+        for link, dest in self.css_links.items():
+            print(" ", dest)
+            urllib.request.urlretrieve(link, os.path.join(self.output_directory, dest))
+
+    def download_images(self):
+        print("Downloading images...")
+        query = self.query_allimages.copy()
+        for images_snippet in self.query_continue(query):
+            for image in images_snippet["allimages"]:
+                title = image["title"]
+                timestamp = self.wiki.parse_date(image["timestamp"])
+                if self.needs_update(title, timestamp):
+                    print("  [downloading] %s" % title)
+                    url = image["url"]
+                    urllib.request.urlretrieve(url, os.path.join(self.output_directory, title))
+                else:
+                    print("  [up-to-date]  %s" % title)
--- arch-wiki-docs.py
+++ arch-wiki-docs.py
@@ -1,124 +1,9 @@
 #! /usr/bin/env python
 
-import os.path
-import urllib.request
 import datetime
 import argparse
 
-from simplemediawiki import MediaWiki
-
-from ArchWikiOfflineOptimizer import ArchWikiOfflineOptimizer
-
-query_allpages = {
-    "action": "query",
-    "generator": "allpages",
-    "gaplimit": "500",
-    "gapfilterredir": "nonredirects",
-    "gapnamespace": "0",
-    "prop": "info",
-    "inprop": "url",
-    "continue": "",
-}
-
-query_allimages = {
-    "action": "query",
-    "list": "allimages",
-    "ailimit": "500",
-    "aiprop": "url|timestamp",
-    "aimaxsize": "10000",
-    "continue": "",
-}
-
-css_links = {
-    "https://wiki.archlinux.org/load.php?debug=false&lang=en&modules=mediawiki.legacy.commonPrint%2Cshared%7Cskins.archlinux&only=styles&skin=archlinux&*": "ArchWikiOffline.css",
-}
-
-def query_continue(query):
-    while True:
-        result = wiki.call(query)
-        if "error" in result:
-            raise Exception(result["error"])
-        if "warnings" in result:
-            print(result["warnings"])
-        if "query" in result:
-            yield result["query"]
-        if "continue" not in result:
-            break
-        query.update(result["continue"])
-
-def print_namespaces():
-    nsmap = wiki.namespaces()
-    nsmap[0] = "Main"   # force main namespace to have name instead of empty string
-    print("Available namespaces:")
-    for ns in sorted(nsmap.keys()):
-        print("  %2d -- %s" % (ns, nsmap[ns]))
-
-# return file name where the given page should be stored
-def get_local_filename(title):
-    # MediaWiki treats uses '_' in links instead of ' '
-    title = title.replace(" ", "_")
-
-    # handle anomalous titles beginning with '/' (e.g. "/dev/shm")
-    if title.startswith("/"):
-        title = title[1:]
-
-    # pages from File namespace already have an extension
-    if not title.startswith("File:"):
-        title += ".html"
-
-    return os.path.join(output_directory, title)
-
-# determine if it is necessary to download a page
-def needs_update(title, timestamp):
-    fname = get_local_filename(title)
-    if not os.path.exists(fname):
-        return True
-    local = datetime.datetime.utcfromtimestamp(os.path.getmtime(fname))
-    if local < timestamp or local < epoch:
-        return True
-    return False
-
-def process_namespace(namespace):
-    print("Processing namespace %s..." % namespace)
-
-    query = query_allpages.copy()
-    query["gapnamespace"] = namespace
-    for pages_snippet in query_continue(query):
-        for page in pages_snippet["pages"].values():
-            title = page["title"]
-            timestamp = wiki.parse_date(page["touched"])
-            if needs_update(title, timestamp):
-                print("  [downloading] %s" % title)
-                fullurl = page["fullurl"]
-
-                # FIXME: this is hack to avoid weird caching issues on ArchWiki
-                fullurl += "?printable=yes"
-
-                html = urllib.request.urlopen(fullurl)
-                awoo = ArchWikiOfflineOptimizer(html, get_local_filename(title), output_directory)
-                awoo.optimize()
-            else:
-                print("  [up-to-date]  %s" % title)
-
-def download_css():
-    print("Downloading CSS...")
-    for link, dest in css_links.items():
-        print(" ", dest)
-        urllib.request.urlretrieve(link, os.path.join(output_directory, dest))
-
-def download_images():
-    print("Downloading images...")
-    query = query_allimages.copy()
-    for images_snippet in query_continue(query):
-        for image in images_snippet["allimages"]:
-            title = image["title"]
-            timestamp = wiki.parse_date(image["timestamp"])
-            if needs_update(title, timestamp):
-                print("  [downloading] %s" % title)
-                url = image["url"]
-                urllib.request.urlretrieve(url, os.path.join(output_directory, title))
-            else:
-                print("  [up-to-date]  %s" % title)
+from ArchWikiDownloader import ArchWikiDownloader
     
 if __name__ == "__main__":
     aparser = argparse.ArgumentParser(description="Download pages from Arch Wiki and optimize them for offline browsing")
@@ -126,22 +11,16 @@ if __name__ == "__main__":
     aparser.add_argument("--force", action="store_true", help="ignore timestamp, always download the page from the wiki")
 
     args = aparser.parse_args()
-    output_directory = args.output_directory
     if args.force:
         epoch = datetime.datetime.utcnow()
     else:
         # this should be the date of the latest incompatible change
         epoch = datetime.datetime(2014, 3, 20)
 
-    # ensure output directory always exists
-    if not os.path.isdir(output_directory):
-        os.mkdir(output_directory)
-
-
-    wiki = MediaWiki("https://wiki.archlinux.org/api.php")
-    print_namespaces()
+    downloader = ArchWikiDownloader("https://wiki.archlinux.org/api.php", args.output_directory, epoch)
+    downloader.print_namespaces()
     for ns in ["0", "4", "12", "14"]:
-        process_namespace(ns)
+        downloader.process_namespace(ns)
 
-    download_images()
-    download_css()
+    downloader.download_images()
+    downloader.download_css()
