ca24dadaaa82120028e837c760bc5dfde69bbefa
Jakub Klinkovský <j.l.k@gmx.com>
2014-04-04 22:13:10 +0200

    added option to clean unwanted files (fixes #7)

--- arch-wiki-docs.py
+++ arch-wiki-docs.py
@@ -9,15 +9,16 @@ from ArchWikiDownloader import ArchWikiDownloader
     
 if __name__ == "__main__":
     aparser = argparse.ArgumentParser(description="Download pages from Arch Wiki and optimize them for offline browsing")
-    aparser.add_argument("--output-directory", type=str, required=True, help="where to store downloaded pages")
-    aparser.add_argument("--force", action="store_true", help="ignore timestamp, always download the page from the wiki")
+    aparser.add_argument("--output-directory", type=str, required=True, help="Path where the downloaded pages should be stored.")
+    aparser.add_argument("--force", action="store_true", help="Ignore timestamp, always download the page from the wiki.")
+    aparser.add_argument("--clean", action="store_true", help="Clean the output directory after downloading, useful for removing pages deleted/moved on the wiki. Warning: any unknown files found in the output directory will be deleted!")
 
     args = aparser.parse_args()
     if args.force:
         epoch = datetime.datetime.utcnow()
     else:
         # this should be the date of the latest incompatible change
-        epoch = datetime.datetime(2014, 3, 20)
+        epoch = datetime.datetime(2014, 4, 4)
 
     aw = ArchWiki()
     optimizer = ArchWikiOptimizer(aw, args.output_directory)
@@ -29,3 +30,6 @@ if __name__ == "__main__":
 
     downloader.download_images()
     downloader.download_css()
+
+    if args.clean:
+        downloader.clean_output_directory()
