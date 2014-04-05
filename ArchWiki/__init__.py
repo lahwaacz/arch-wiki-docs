5105db4546d633de02324dba648f9b86b2d987f6
Jakub Klinkovský <j.l.k@gmx.com>
2014-04-05 10:48:21 +0200

    version 0.2

--- ArchWiki/__init__.py
+++ ArchWiki/__init__.py
@@ -2,7 +2,7 @@
 
 __author__ = "Jakub Klinkovský <j.l.k@gmx.com>"
 __url__ = "https://github.com/lahwaacz/arch-wiki-docs"
-__version__ = "0.1"
+__version__ = "0.2"
 
 from .ArchWiki import *
 from .downloader import *
