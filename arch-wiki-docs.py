f06607e0fae6c45608d2f1a0cceed0e383a17f90
kiasoc5 <kiasoc5@disroot.org>
2022-05-07 04:07:38 -0400

    Support downloading specific languages
    fixes #9

--- arch-wiki-docs.py
+++ arch-wiki-docs.py
@@ -2,10 +2,12 @@
 
 import datetime
 import argparse
+import sys
 
 from simplemediawiki import build_user_agent
 
 import ArchWiki
+from ArchWiki.ArchWiki import language_names
     
 if __name__ == "__main__":
     aparser = argparse.ArgumentParser(description="Download pages from Arch Wiki and optimize them for offline browsing")
@@ -13,8 +15,15 @@ if __name__ == "__main__":
     aparser.add_argument("--force", action="store_true", help="Ignore timestamp, always download the page from the wiki.")
     aparser.add_argument("--clean", action="store_true", help="Clean the output directory after downloading, useful for removing pages deleted/moved on the wiki. Warning: any unknown files found in the output directory will be deleted!")
     aparser.add_argument("--safe-filenames", action="store_true", help="Force using ASCII file names instead of the default Unicode.")
+    aparser.add_argument("--langs", type=str, nargs='+', help="Download only pages of specified languages")
+    aparser.add_argument("--list-langs", action="store_true", help="List supported languages")
 
     args = aparser.parse_args()
+    if args.list_langs:
+        for lang in language_names.values():
+            print(lang['subtag'], lang['english'])
+        sys.exit()
+
     if args.force:
         epoch = datetime.datetime.utcnow()
     else:
@@ -22,7 +31,7 @@ if __name__ == "__main__":
         epoch = datetime.datetime(2016, 3, 3, 18, 0, 0)
 
     user_agent = build_user_agent(__file__, ArchWiki.__version__, ArchWiki.__url__)
-    aw = ArchWiki.ArchWiki(user_agent=user_agent, safe_filenames=args.safe_filenames)
+    aw = ArchWiki.ArchWiki(user_agent=user_agent, safe_filenames=args.safe_filenames, langs=args.langs)
     optimizer = ArchWiki.Optimizer(aw, args.output_directory)
 
     downloader = ArchWiki.Downloader(aw, args.output_directory, epoch, optimizer=optimizer)
