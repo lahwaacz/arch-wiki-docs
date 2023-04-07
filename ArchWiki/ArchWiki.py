lahwaacz/arch-wiki-docs

commit f06607e0fae6c45608d2f1a0cceed0e383a17f90

Support downloading specific languages
fixes #9

ArchWiki/ArchWiki.py
@@ -97,7 +97,7 @@ def is_ascii(text):
 
 class ArchWiki(MediaWiki):
 
-    def __init__(self, safe_filenames=False, **kwargs):
+    def __init__(self, safe_filenames=False, langs=None, **kwargs):
         """ Parameters:
             @safe_filenames: force self.get_local_filename() to return ASCII string
             + all keyword arguments of simplemediawiki.MediaWiki
@@ -107,6 +107,14 @@ class ArchWiki(MediaWiki):
         self._safe_filenames = safe_filenames
         self._namespaces = None
         self._redirects = None
+
+        if langs is not None:
+            self._language_names = {}
+            for lang, metadata in language_names.items():
+                if not set(metadata.values()).isdisjoint(langs):
+                    self._language_names[lang] = metadata
+        else:
+            self._language_names = language_names
 
     def query_continue(self, query):
         """ Generator for MediaWiki's query-continue feature.
@@ -168,15 +176,21 @@ class ArchWiki(MediaWiki):
         match = re.match("^(.+?)([ _]\(([^\(]+)\))?$", title);
         if match:
             lang = match.group(3)
-            if lang in language_names:
+            if lang in self._language_names:
                 detected_language = lang
                 pure_title = match.group(1)
+            else:
+                detected_language = None
         return pure_title, detected_language
 
     def get_local_filename(self, title, basepath):
         """ Return file name where the given page should be stored, relative to 'basepath'.
         """
         title, lang = self.detect_language(title)
+
+        if lang is None:
+            return None
+
         title, namespace = self.detect_namespace(title)
 
         # be safe and use '_' instead of ' ' in filenames (MediaWiki style)
@@ -201,7 +215,7 @@ class ArchWiki(MediaWiki):
 
         path = pattern.format(
             base=basepath,
-            langsubtag=language_names[lang]["subtag"],
+            langsubtag=self._language_names[lang]["subtag"],
             namespace=namespace,
             title=title,
             ext="html"

ArchWiki/downloader.py
@@ -82,6 +82,9 @@ class Downloader:
             for page in sorted(pages_snippet["pages"].values(), key=lambda d: d["title"]):
                 title = page["title"]
                 fname = self.wiki.get_local_filename(title, self.output_directory)
+                if not fname:
+                    print(f"  [skipping] {title}")
+                    continue
                 self.files.append(fname)
                 timestamp = self.wiki.parse_date(page["touched"])
                 if self.needs_update(fname, timestamp):
@@ -107,10 +110,11 @@ class Downloader:
         for link, dest in self.css_links.items():
             print(" ", dest)
             fname = os.path.join(self.output_directory, dest)
-            self.files.append(fname)
-            r = self.session.get(link)
-            with open(fname, "w") as fd:
-                fd.write(r.text)
+            if fname:
+                self.files.append(fname)
+                r = self.session.get(link)
+                with open(fname, "w") as fd:
+                    fd.write(r.text)
 
     def download_images(self):
         print("Downloading images...")
@@ -119,6 +123,9 @@ class Downloader:
             for image in images_snippet["allimages"]:
                 title = image["title"]
                 fname = self.wiki.get_local_filename(title, self.output_directory)
+                if not fname:
+                    print(f"  [skipping] {title}")
+                    continue
                 self.files.append(fname)
                 timestamp = self.wiki.parse_date(image["timestamp"])
                 if self.needs_update(fname, timestamp):

arch-wiki-docs.py
@@ -2,10 +2,12 @@
 
 import datetime
 import argparse
+import sys
 
 from simplemediawiki import build_user_agent
 
 import ArchWiki
+from ArchWiki.ArchWiki import language_names
     
 if __name__ == "__main__":
     aparser = argparse.ArgumentParser(description="Download pages from Arch Wiki and optimize them for offline browsing")
@@ -13,8 +15,15 @@ if __name__ == "__main__":
     aparser.add_argument("--force", action="store_true", help="Ignore timestamp, always download the page from the wiki.")
     aparser.add_argument("--clean", action="store_true", help="Clean the output directory after downloading, useful for removing pages deleted/moved on the wiki. Warning: any unknown files found in the output directory will be deleted!")
     aparser.add_argument("--safe-filenames", action="store_true", help="Force using ASCII file names instead of the default Unicode.")
+    aparser.add_argument("--langs", type=str, nargs='+', help="Download only pages of specified languages")
+    aparser.add_argument("--list-langs", action="store_true", help="List supported languages")
 
     args = aparser.parse_args()
+    if args.list_langs:
+        for lang in language_names.values():
+            print(lang['subtag'], lang['english'])
+        sys.exit()
+
     if args.force:
         epoch = datetime.datetime.utcnow()
     else:
@@ -22,7 +31,7 @@ if __name__ == "__main__":
         epoch = datetime.datetime(2016, 3, 3, 18, 0, 0)
 
     user_agent = build_user_agent(__file__, ArchWiki.__version__, ArchWiki.__url__)
-    aw = ArchWiki.ArchWiki(user_agent=user_agent, safe_filenames=args.safe_filenames)
+    aw = ArchWiki.ArchWiki(user_agent=user_agent, safe_filenames=args.safe_filenames, langs=args.langs)
     optimizer = ArchWiki.Optimizer(aw, args.output_directory)
 
     downloader = ArchWiki.Downloader(aw, args.output_directory, epoch, optimizer=optimizer)

commit 929424410caecd0265e35126f625d9d1a7a63625

fixed language filter bugs
- the commit f06607e0fae6c45608d2f1a0cceed0e383a17f90 broke the
  optimizer for cases where a downloaded page linked to a skipped page
- the commit 2d830eedea6221ca35537e2237aa2bdcc8c41d94 broke the logic
  introduced in the aforementioned commit - now the filtering by
  language is done outside the detect_language function

ArchWiki/ArchWiki.py
@@ -163,7 +163,7 @@ class ArchWiki(MediaWiki):
         if match:
             pure = match.group("pure")
             lang = match.group("lang")
-            if lang in self._language_names:
+            if lang in language_names:
                 # strip "(Language)" from all subpage components to handle cases like
                 # "Page name (Language)/Subpage (Language)"
                 if strip_all_subpage_parts is True and "/" in pure:
@@ -188,7 +188,7 @@ class ArchWiki(MediaWiki):
         """
         title, lang = self.detect_language(title)
 
-        if lang is None:
+        if lang not in self._language_names:
             return None
 
         title, namespace = self.detect_namespace(title)

ArchWiki/optimizer.py
@@ -98,6 +98,9 @@ class Optimizer:
                     if match.group(2):
                         fragment = match.group(2)
                     href = self.wiki.get_local_filename(title, relbase)
+                    # get_local_filename returns None for skipped pages
+                    if href is None:
+                        continue
                     if fragment:
                         href += "#" + fragment
                     a.set("href", href)